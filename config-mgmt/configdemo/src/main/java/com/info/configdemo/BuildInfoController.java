7181b39e499408126c12d144a50afe7ff4c61e5a
leoga <leoga666@hotmail.com>
2026-03-14 17:09:31 +0100

    Ads Grafana with Loki log monitoring for all the apps in the project

--- config-mgmt/configdemo/src/main/java/com/info/configdemo/BuildInfoController.java
+++ config-mgmt/configdemo/src/main/java/com/info/configdemo/BuildInfoController.java
@@ -1,6 +1,7 @@
 package com.info.configdemo;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @AllArgsConstructor
 @RefreshScope
+@Slf4j
 public class BuildInfoController {
 
 //    @Value("${build.id:default}")
@@ -23,6 +25,11 @@ public class BuildInfoController {
 
     @GetMapping("/build-info")
     public String getBuildInfo() {
+        log.trace("Build ID: {}, Version: {}, Name: {}", buildInfo.getId(), buildInfo.getVersion(), buildInfo.getName());
+        log.debug("Build ID: {}, Version: {}, Name: {}", buildInfo.getId(), buildInfo.getVersion(), buildInfo.getName());
+        log.info("Build ID: {}, Version: {}, Name: {}", buildInfo.getId(), buildInfo.getVersion(), buildInfo.getName());
+        log.warn("Build ID: {}, Version: {}, Name: {}", buildInfo.getId(), buildInfo.getVersion(), buildInfo.getName());
+        log.error("Build ID: {}, Version: {}, Name: {}", buildInfo.getId(), buildInfo.getVersion(), buildInfo.getName());
 //        return "Build ID: " + buildId + ", Version: " + buildVersion + ", Name: " + buildName;
         return "Build ID: " + buildInfo.getId() + ", Version: " + buildInfo.getVersion() + ", Name: " + buildInfo.getName();
     }
--- interservice/provider/src/main/java/com/example/provider/InstanceController.java
+++ interservice/provider/src/main/java/com/example/provider/InstanceController.java
@@ -1,10 +1,12 @@
 package com.example.provider;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class InstanceController {
 
     @Value("${server.port}")
@@ -14,6 +16,11 @@ public class InstanceController {
 
     @GetMapping("/instance-info")
     public String getInstanceInfo(){
+        log.trace("Request received at instance running on port: {}", port);
+        log.debug("Request received at instance running on port: {}", port);
+        log.info("Request received at instance running on port: {}", port);
+        log.warn("Request received at instance running on port: {}", port);
+        log.error("Request received at instance running on port: {}", port);
         System.out.println("Request received at instance running on port: " + port);
         return "Instance served by port:" + port + ". Instance ID: " + instanceId;
     }
